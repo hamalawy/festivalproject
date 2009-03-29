d4f8d5e5b3af14dd0c5c9a9bcd1235c52f9cf712
geromequentin <geromequentin@61915262-1b08-11de-b650-e715bd6d7cf1>
2009-03-29 20:22:40 +0000

    Thread opérationnel

--- src/View/ThreadBarreInfo.java
+++ src/View/ThreadBarreInfo.java
@@ -2,45 +2,37 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package View;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Cramike
  */
-public class ThreadBarreInfo extends Thread{
+public class ThreadBarreInfo extends Thread {
+
     private BarreInfo barreInfo;
     private long time;
-    public ThreadBarreInfo(BarreInfo barreInfo)
-    {
+
+    public ThreadBarreInfo(BarreInfo barreInfo) {
         this.barreInfo = barreInfo;
         this.setPriority(Thread.MIN_PRIORITY);
     }
 
     @Override
-    public void run()
-    {
-            System.out.println("Entree dans le thread");
-            try {
-                Thread.sleep(2000);
-            }
-            catch (IllegalThreadStateException il)
-            {
-                System.out.println("Erreur de illegalThreadStateException");
-            }
-            catch (InterruptedException ex) {
-                System.out.println("Erreur de thread");
-                System.exit(0);
-            }
-        System.out.println("Sortie du thread");
-        barreInfo.getText().setText(null);
-
-        this.suspend();
-
-
+    public void run() {
+        while (true) {
+            if (barreInfo.isRequestReceived()) {
+                try {
+                    Thread.sleep(3000);
+                    barreInfo.setThreadText();
+                } catch (IllegalThreadStateException il) {
+                    System.out.println("Erreur de illegalThreadStateException");
+                } catch (InterruptedException ex) {
+                    System.out.println("Erreur de thread");
+                    System.exit(0);
+                }
+            } 
+        }
     }
 }
