608221da997fdfd55d645634f601879719de54cb
geromequentin <geromequentin@61915262-1b08-11de-b650-e715bd6d7cf1>
2009-03-29 20:22:23 +0000

    J'ai retiré le "lancement" et ajouté le support du thread

--- trunk/src/View/BarreInfo.java
+++ trunk/src/View/BarreInfo.java
@@ -1,37 +1,40 @@
 package View;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
 public class BarreInfo extends JPanel {
-	private JLabel texte;
-	public BarreInfo()
-	{
-		this.setLayout(new GridLayout(1,2));
-		texte = new JLabel();
-		add(texte);
-        setLancement();
-	}
-	
-	
-	public JLabel getText()
-	{
-		return texte;
-	}
-	
-	
-	public void setText(String str)
-	{
-        this.texte.setText(str);
 
-	}
-    public void setLancement()
-    {
-        this.texte.setText("Lancement...");
+    private JLabel texte;
+    private boolean requestReceived;
+    private ThreadBarreInfo thread = new ThreadBarreInfo(this);
+
+    public BarreInfo() {
+        thread.start();
+        this.setLayout(new GridLayout(1, 2));
+        this.setPreferredSize(new Dimension(20,30));
+        texte = new JLabel();
+        add(texte);
+    }
+
+    public boolean isRequestReceived() {
+        return requestReceived;
+    }
+
+    public JLabel getText() {
+        return texte;
+    }
+
+    public void setText(String str) {
+        this.texte.setText(str);
+        requestReceived = true;
 
     }
 
+    public void setThreadText() {
+        setText("");
+    }
 }
