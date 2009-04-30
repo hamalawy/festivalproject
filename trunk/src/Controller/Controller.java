2b693aa594061e93da6aaac4e85c791dc3ac5005
geromequentin <geromequentin@61915262-1b08-11de-b650-e715bd6d7cf1>
2009-04-30 17:50:15 +0000

    Version fonctionnelle juste avant le controle :D

--- trunk/src/Controller/Controller.java
+++ trunk/src/Controller/Controller.java
@@ -5,9 +5,14 @@ import Data.BDException;
 import Data.GestionGroupeBD;
 import Data.GestionStandBD;
 import Data.Groupe;
+import Data.GroupeID;
 import Data.Stand;
 import Data.LoginException;
 import java.util.Vector;
+import View.groupe.GroupeNotAcceptedException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class Controller {
 
@@ -17,13 +22,31 @@ public class Controller {
         gs.createStand(newStand);
     }
 
-    public static void createGroupe(Groupe newGroupe, Vector<MembreGroupe> vectMembreGroupe) throws BDException, LoginException {
+    public static void createGroupe(Groupe newGroupe) throws GroupeNotAcceptedException {
         // TODO Auto-generated method stub
         GestionGroupeBD gs = new GestionGroupeBD();
-        gs.createGroupe(newGroupe, vectMembreGroupe);
+        try {
+            gs.createGroupe(newGroupe);
+        } catch (BDException ex) {
+            throw new GroupeNotAcceptedException(ex);
+        } catch (LoginException ex) {
+            throw new GroupeNotAcceptedException(ex);
+        } 
+            
     }
 
-    public static void deleteMembreGroupe(MembreGroupe membre) {
+    public static void addMembreToGroupe(GroupeID groupe, MembreGroupe membre) throws GroupeNotAcceptedException{
+        try {
+            GestionGroupeBD gs = new GestionGroupeBD();
+            gs.addMembreToGroupe(groupe, membre);
+        } catch (BDException ex) {
+            throw new GroupeNotAcceptedException(ex);
+        } catch (LoginException ex) {
+            throw new GroupeNotAcceptedException(ex);
+        }
+    }
+
+    public static void deleteMembreGroupe(MembreGroupe membre) throws BDException, GroupeNotAcceptedException {
         GestionGroupeBD gs = new GestionGroupeBD();
         gs.deleteMembreGroupe(membre);
     }
@@ -33,9 +56,9 @@ public class Controller {
         return gs.getAllGenre();
     }
 
-    public static Vector<MembreGroupe> getAllMembreGroupe() throws BDException, LoginException {
+    public static Vector<MembreGroupe> getAllMembreGroupe(GroupeID groupeSelected) throws BDException, LoginException {
         GestionGroupeBD gs = new GestionGroupeBD();
-        return gs.getAllMembreGroupe();
+        return gs.getAllMembreGroupe(groupeSelected);
     }
 
     public static Vector<String> getAllGroupeNationalite() throws BDException, LoginException {
@@ -46,5 +69,25 @@ public class Controller {
     public static Vector<String> getAllMembreGroupeNationalite() throws BDException, LoginException {
         GestionGroupeBD gg = new GestionGroupeBD();
         return gg.getAllMembreGroupeNationalite();
+    }
+
+    public static Vector<String> getAllInstruments() throws BDException, LoginException {
+        GestionGroupeBD gg = new GestionGroupeBD();
+        return gg.getAllInstrument();
+    }
+
+    public static Vector<GroupeID> getAllGroupeForList() throws BDException, LoginException {
+        GestionGroupeBD gg = new GestionGroupeBD();
+        return gg.getAllGroupeForList();
+    }
+
+    public static Groupe getGroupeSelected(GroupeID nomGroupeSelected) throws BDException, LoginException {
+        GestionGroupeBD gg = new GestionGroupeBD();
+        return gg.getGroupeSelected(nomGroupeSelected);
+    }
+
+    public static Vector<String> getAllMembreForCombo(GroupeID groupeSelected) throws BDException, LoginException {
+        GestionGroupeBD gg = new GestionGroupeBD();
+        return gg.getAllMembreForCombo(groupeSelected);
     }
 }
