d8647ae30594e434bdbac72541acbc275722a790
geromequentin <geromequentin@61915262-1b08-11de-b650-e715bd6d7cf1>
2009-04-30 17:50:31 +0000

    Version fonctionnelle juste avant le controle :D

--- src/Data/ConnexBD.java
+++ src/Data/ConnexBD.java
@@ -1,33 +1,33 @@
 package Data;
 
+import View.ConnexPanel;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.swing.JOptionPane;
 
 public class ConnexBD {
+    private static String nomDB, pass;
+    private static Connection uniqueInstance;
 
-	private static Connection uniqueInstance;
-	private static DemandeConnexBDPanel demandeConnex;
-	public static Connection getInstance() throws BDException, LoginException
-	{
-		if(uniqueInstance == null)
-		{
-            demandeConnex = new DemandeConnexBDPanel();
-			JOptionPane.showMessageDialog(null, demandeConnex, "Connexion BD", JOptionPane.INFORMATION_MESSAGE);
-			try {
-				Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-                if(demandeConnex.isCorrectUser())
-                    uniqueInstance = DriverManager.getConnection("jdbc:odbc:festival", demandeConnex.getUser(), demandeConnex.getPassword());
-                else
-                    ConnexBD.getInstance();
+
+    public static Connection getInstance() throws BDException, LoginException {
+        return getInstance(nomDB, pass);
+    }
+
+    public static Connection getInstance(String nomDB, String pass) throws BDException, LoginException {
+        ConnexBD.nomDB = nomDB;
+        ConnexBD.pass = pass;
+        if (uniqueInstance == null) {
+            try {
+                Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+                uniqueInstance = DriverManager.getConnection("jdbc:odbc:"+nomDB, "Crail", pass);
             } catch (ClassNotFoundException e) {
-				throw new BDException(e);
-			} catch (SQLException e) {
-				throw new BDException(e);
-			}
-		}
-		return uniqueInstance;
-	}
+                throw new BDException(e);
+            } catch (SQLException e) {
+                throw new BDException(e);
+            }
+        }
+        return uniqueInstance;
+    }
 }
---
+++ src/Data/GroupeID.java
@@ -0,0 +1,18 @@
+
+package Data;
+
+
+/**
+ *
+ * @author Gérôme Quentin
+ * @author Scaillet Bruno
+ */
+public class GroupeID extends Groupe{
+
+
+    public GroupeID(String nom, String nationalite) {
+        super(nom, nationalite,null,null,null,null,null);
+    }
+
+    
+}
