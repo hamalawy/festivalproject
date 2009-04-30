6aa92133001e0f50081f789dadd47c125b683745
geromequentin <geromequentin@61915262-1b08-11de-b650-e715bd6d7cf1>
2009-04-30 17:52:36 +0000

    Version fonctionnelle juste avant le controle :D

---
+++ trunk/src/View/groupe/GroupeInscrMembrePanel.java
@@ -0,0 +1,122 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View.groupe;
+
+import Data.MembreGroupe;
+import Data.LoginException;
+import View.DateException;
+import View.GestionFocusTextField;
+import Controller.Controller;
+import Data.BDException;
+import View.DatePanel;
+
+import java.awt.Dimension;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Cramike
+ */
+public class GroupeInscrMembrePanel extends InscrMembrePanel {
+
+    private InscriptionMembreActionPanel actionPanel;
+    private GroupeInscriptionMainPanel groupePanel;
+
+    public GroupeInscrMembrePanel(GroupeInscriptionMainPanel groupePanel) {
+        super(groupePanel.getBarreInfo());
+        this.groupePanel = groupePanel;
+        actionPanel = new InscriptionMembreActionPanel(this);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 5;
+        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        add(actionPanel, gridBagConstraints);
+
+        comboNationalite.setEditable(true);
+        try {
+            comboNationalite.setModel(new javax.swing.DefaultComboBoxModel(Controller.getAllMembreGroupeNationalite()));
+        } catch (BDException ex) {
+            groupePanel.setInfoText(ex.toString());
+        } catch (LoginException le) {
+            groupePanel.setInfoText(le.toString());
+        }
+
+        comboNationalite.setMaximumSize(new java.awt.Dimension(110, 20));
+        comboNationalite.setMinimumSize(new java.awt.Dimension(110, 20));
+        comboNationalite.setPreferredSize(new java.awt.Dimension(110, 20));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 5, 5);
+        add(comboNationalite, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 5, 10);
+        add(datePanel, gridBagConstraints);
+
+        comboInstrument.setEditable(true);
+        try {
+            comboInstrument.setModel(new javax.swing.DefaultComboBoxModel(Controller.getAllInstruments()));
+        } catch (BDException ex) {
+            groupePanel.setInfoText(ex.toString());
+        } catch (LoginException le) {
+            groupePanel.setInfoText(le.toString());
+        }
+        comboInstrument.setMinimumSize(new java.awt.Dimension(110, 20));
+        comboInstrument.setPreferredSize(new java.awt.Dimension(110, 20));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 5, 10);
+        add(comboInstrument, gridBagConstraints);
+
+        comboDomaine.setModel(new javax.swing.DefaultComboBoxModel(new String[]{"Technicien", "Musicien"}));
+        comboDomaine.setMinimumSize(new java.awt.Dimension(110, 20));
+        comboDomaine.setPreferredSize(new java.awt.Dimension(110, 20));
+        comboDomaine.setSelectedIndex(1);
+        comboDomaine.addItemListener(comboListener);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 5, 5);
+        add(comboDomaine, gridBagConstraints);
+    }
+
+    public JButton getButAjouter() {
+        return actionPanel.getButAjouter();
+    }
+
+    void actionButAjouterNouveau() {
+        try {
+            MembreGroupe newMembre = this.getMembre();
+            groupePanel.getVectMembreGroupe().add(newMembre);
+            groupePanel.afficherAjoutMembre();
+        } catch (DateException ex) {
+            groupePanel.setInfoText("Date incorrecte");
+        }
+    }
+
+    public void actionButAjouter() {
+        try {
+            MembreGroupe newMembre = this.getMembre();
+            groupePanel.getVectMembreGroupe().add(newMembre);
+            groupePanel.afficherTableMembre();
+        } catch (DateException ex) {
+            groupePanel.setInfoText("Date incorrecte");
+        }
+    }
+
+    public GroupeInscriptionMainPanel getGroupePanel() {
+        return groupePanel;
+    }
+}
